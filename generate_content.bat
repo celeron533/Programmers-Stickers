242d682bcac9c3b925d5df7357e039d0192eaecc
celeron533 <zhuyilun8@hotmail.com>
2018-08-27 22:30:03 +0800

    Set image size to 240x240

--- generate_content.bat
+++ generate_content.bat
@@ -1,1 +1,1 @@
-FOR /R .\source %%I IN (*.txt) DO magick -size 320x320 -font SimHei label:@source\\%%~nxI output\\%%~nI.png
+FOR /R .\source %%I IN (*.txt) DO magick -size 240x240 -font SimHei label:@source\\%%~nxI output\\%%~nI.png
